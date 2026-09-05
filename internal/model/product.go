e5c8cd55d152454eabff5eb603dedd3017ec0bab
monupanwar1 <monupanwar5655@gmail.com>
2026-09-05 21:47:54 +0530

    feat: add product model and database setup

---
+++ internal/model/product.go
@@ -0,0 +1,9 @@
+package model
+
+type Product struct {
+	ID          uint    `gorm:"primaryKey" json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Image       string  `json:"image"`
+}
