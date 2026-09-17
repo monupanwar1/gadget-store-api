f0a53d41fe5711115307f6e7bfe0785cb7dc540c
monupanwar1 <monupanwar5655@gmail.com>
2026-09-17 22:15:30 +0530

    feat: add category model

--- internal/model/product.go
+++ internal/model/product.go
@@ -1,9 +1,23 @@
 package model
 
+import "time"
+
+type Category struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Name      string    `json:"name"`
+	Slug      string    `json:"slug"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 type Product struct {
-	ID          uint    `gorm:"primaryKey" json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	Image       string  `json:"image"`
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	Image       string    `json:"image"`
+	CategoryID  uint      `json:"category_id"`
+	Category    Category  `json:"category"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
